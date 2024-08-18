7a69a5be82b03bafeea77e1c8eed077ab8a63844
Selim Jacquet <iamselimj@proton.me>
2024-08-19 00:20:22 +0200

    fix:background icons

--- src/components/certification-card.tsx
+++ src/components/certification-card.tsx
@@ -25,7 +25,7 @@ export function CertificationCard({
   return (
     <li className="relative ml-10 py-4">
       <div className="absolute -left-16 top-2 flex items-center justify-center">
-        <Avatar className="">
+        <Avatar className="bg-background">
           <AvatarImage src={image} alt={title} className="object-fit" />
           <AvatarFallback>{title[0]}</AvatarFallback>
         </Avatar>
