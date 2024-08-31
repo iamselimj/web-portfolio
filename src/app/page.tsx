517a623a907be3220aad6701ee0083e47e1785e6
Selim Jacquet <iamselimj@proton.me>
2024-08-31 16:07:13 +0200

    update:contact text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -195,7 +195,7 @@ export default function Page() {
                 Get in Touch
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                blablabla
+                Work in progress.
               </p>
             </div>
           </BlurFade>
